1f92f9426b41aa9b046229652931066ffa2bd363
/HM2ewKX2zIHwNYoasWrC6HOD9h7peZ12XP6eaOTa0M= <torresaresluis@gmail.com>
2025-03-05 11:12:45 +0100

    Eliminados scripts no necesarios para esta versión

--- scripts/6-swarm.sh
+++
@@ -1,2 +0,0 @@
-swarm -d 1 -i ./spades_output/contigs.fasta -o ./swarm/swarm_output.txt -s ./swarm/swarm_output.stats
-vsearch --fastx_getseqs ./spades_output/contigs.fasta --labels $(awk '{print $1}' ./swarm/swarm_output.txt) --output ./swarm/swarm_representatives.fasta
--- scripts/8-blast.sh
+++
@@ -1,5 +0,0 @@
-#Convertir la referencia (k-mers) en una base de datos compatible con blast
-makeblastdb -in reference.fasta -dbtype nucl -out reference_db
-
-#Blast
-blastn -query ./swarm/swarm_representatives.fasta -db reference_db -out blast_output.txt -outfmt "10 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
